8102b87d60ad3bfe24753fd0216293406d42fba3
SameetKeshav <u21479373@tuks.co.za>
2023-05-12 16:55:03 +0200

    Added Ionic

--- apps/encompass/src/app/app.module.ts
+++ apps/encompass/src/app/app.module.ts
@@ -4,12 +4,14 @@ import { RouterModule } from '@angular/router';
 import { AppComponent } from './app.component';
 import { appRoutes } from './app.routes';
 import { NxWelcomeComponent } from './nx-welcome.component';
+import { IonicModule } from '@ionic/angular';
 
 @NgModule({
   declarations: [AppComponent, NxWelcomeComponent],
   imports: [
     BrowserModule,
     RouterModule.forRoot(appRoutes, { initialNavigation: 'enabledBlocking' }),
+    IonicModule.forRoot(),
   ],
   providers: [],
   bootstrap: [AppComponent],
