dda95f87d02f4eeb526f4019cd0c2b79c3c278c0
RoninBrookes <126901461+RoninBrookesTuks@users.noreply.github.com>
2023-05-21 20:19:23 +0200

    Sign-Up Interiror Page Created

---
+++ libs/app/sign-up-interior1/feature/src/lib/app-sign-up-interior1-feature.spec.ts
@@ -0,0 +1,9 @@
+import { appSignUpInterior1Feature } from './app-sign-up-interior1-feature';
+
+describe('appSignUpInterior1Feature', () => {
+  it('should work', () => {
+    expect(appSignUpInterior1Feature()).toEqual(
+      'app-sign-up-interior1-feature'
+    );
+  });
+});
