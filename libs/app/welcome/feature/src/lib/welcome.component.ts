828829d0cf2d3406815fe1cfeb561629176cd760
RoninBrookes <126901461+RoninBrookesTuks@users.noreply.github.com>
2023-05-21 19:58:16 +0200

    Implemented the Welcome Page

--- libs/app/welcome/feature/src/lib/welcome.component.ts
+++ libs/app/welcome/feature/src/lib/welcome.component.ts
@@ -3,7 +3,7 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'welcome',
   templateUrl: './welcome.component.html',
-  styleUrls: ['./welcome.component.css']
+  styleUrls: ['./welcome.component.scss']
 })
 export class WelcomeComponent {
 
