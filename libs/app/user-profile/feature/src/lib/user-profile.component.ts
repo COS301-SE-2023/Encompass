7ead5d34c76d74c72424c0ea81c715b3aa0f3f39
SameetKeshav <u21479373@tuks.co.za>
2023-05-29 20:55:24 +0200

    Update user-profile.component.ts

--- libs/app/user-profile/feature/src/lib/user-profile.component.ts
+++ libs/app/user-profile/feature/src/lib/user-profile.component.ts
@@ -3,7 +3,7 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'user-profile',
   templateUrl: './user-profile.component.html',
-  styleUrls: ['./user-profile.component.css']
+  styleUrls: ['./user-profile.component.scss']
 })
 export class UserProfile {
 
