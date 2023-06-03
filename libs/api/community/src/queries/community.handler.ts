7df0056d46fc4f4da27fb392d63c06ddaea14edf
Morgan90-B <u18103007@tuks.co.za>
2023-06-03 15:09:59 +0200

    Create community.handler.ts

---
+++ libs/api/community/src/queries/community.handler.ts
@@ -0,0 +1,18 @@
+import { CommandHandler, ICommandHandler} from "@nestjs/cqrs";
+import { GetCommunityCommand } from "./community.command"
+import { CommunityEntityRepository } from "../db/community-entity.repository";
+import { EventPublisher } from "@nestjs/cqrs";
+
+@CommandHandler(GetCommunityCommand)
+export class GetCommunityHandler implements ICommandHandler<GetCommunityCommand> {
+    constructor(
+        private readonly communityEntityRepository: CommunityEntityRepository,
+        private readonly eventPublisher: EventPublisher,
+    ) {}
+
+    async execute({ name, admin, about, members, events, posts }: GetCommunityCommand) {
+        const community = await this.eventPublisher.mergeObjectContext(
+            await this.communityEntityRepository.findOneById(_id),
+        );
+    }
+}
