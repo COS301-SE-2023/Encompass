57a6f0b9a0cd1e459b3f6f7b98ed0051d7fea897
SameetKeshav <u21479373@tuks.co.za>
2023-06-03 10:12:53 +0200

    Allow MongoDB connection for Windows

--- apps/api/src/app/app.module.ts
+++ apps/api/src/app/app.module.ts
@@ -11,7 +11,7 @@ import { ProfileModule } from '@encompass/api/profile/data-access';
     HomeModule,
     AccountModule,
     ProfileModule,
-    MongooseModule.forRoot('mongodb://localhost:27017/encompass-database')
+    MongooseModule.forRoot('mongodb://127.0.0.1:27017/encompass-database')
   ],
   controllers: [AppController],
   providers: [AppService],
