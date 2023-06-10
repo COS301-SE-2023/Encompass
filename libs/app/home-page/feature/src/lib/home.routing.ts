cb2bd94d8e3c7154013a1713fc7324fa3709fe7d
Julianna-Venter <juliannaventer@gmail.com>
2023-06-10 12:50:36 +0200

    Routing works

--- libs/app/home-page/feature/src/lib/home.routing.ts
+++ libs/app/home-page/feature/src/lib/home.routing.ts
@@ -5,16 +5,11 @@ import { HomePage } from './home-page.component';
 const routes: Routes = [
   {
     path: '',
-    pathMatch: 'full',
-    redirectTo: '/home/feed',
-  },
-  {
-    path: '',
     component: HomePage,
     children: [
       {
-        path: 'home/feed',
-        // match: 'full',
+        path: '',
+        pathMatch: 'full',
         loadChildren: () =>
           import('@encompass/app/feed/feature').then((m) => m.FeedModule),
       },
