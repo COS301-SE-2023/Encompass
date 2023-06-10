2931fdb01d8b1ffcc65b86fba7a7184cbd327661
Morgan90-B <u18103007@tuks.co.za>
2023-06-10 20:49:14 +0200

    Update update-community.handler.ts

---
+++ libs/api/community/data-access/src/commands/update-community.handler.ts
@@ -0,0 +1,24 @@
+import { CommandHandler, EventPublisher, ICommandHandler } from "@nestjs/cqrs";
+import { UpdateCommunityCommand } from "./update-community.command";
+import { CommunityEntityRepository } from "../db/community-entity.repository";
+
+@CommandHandler(UpdateCommunityCommand)
+export class UpdateCommunityHandler
+    implements ICommandHandler<UpdateCommunityCommand> {
+        constructor(
+            private readonly communityEntityRepository: CommunityEntityRepository,
+            private readonly eventPublisher: EventPublisher,
+        ) {}
+
+        async execute({ id, updateCommunityRequest }: UpdateCommunityCommand) {
+            const community = this.eventPublisher.mergeObjectContext(
+                await this.communityEntityRepository.findOneById(id),
+            );
+            
+            community.updateCommunity(updateCommunityRequest);
+            await this.communityEntityRepository.findOneAndReplaceById(id, community);
+            community.commit();
+
+            return community;
+        }
+    }
