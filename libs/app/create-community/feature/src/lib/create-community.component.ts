00f1c4b97386c8c9de336af3f4a187b2b81ac98a
RoninBrookes <126901461+RoninBrookesTuks@users.noreply.github.com>
2023-06-12 14:37:30 +0200

    CreateCommunity logic partially implemented

--- libs/app/create-community/feature/src/lib/create-community.component.ts
+++ libs/app/create-community/feature/src/lib/create-community.component.ts
@@ -1,5 +1,14 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { ModalController } from '@ionic/angular';
+// import './create-post.component.scss';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { CreatePost } from '@encompass/app/create-post/util';
+import { Select, Store } from '@ngxs/store';
+import { ProfileState } from '@encompass/app/profile/data-access';
+import { Observable } from 'rxjs';
+import { ProfileDto } from '@encompass/api/profile/data-access';
+import { SubscribeToProfile } from '@encompass/app/profile/util';
+
 @Component({
   selector: 'create-community',
   templateUrl: './create-community.component.html',
@@ -7,8 +16,40 @@ import { ModalController } from '@ionic/angular';
 })
 export class CreateCommunityComponent {
 
-  constructor(private modalController: ModalController) {}
+  communities: string[] =["Hobbits and all thatsdadwadawda","Community2222"]; // Replace with the actual type of the communities array
+  categories: string[]=["Movies","Mathematics","Action","Science-Fiction"
+  ,"Drama","Romance","Anime","Books","Hospitality","Animation","Documentary"
+  ,"Physics","Geography","Series","Western","Mystery","Fantasy","Life-Science"
+  ,"War","IT","Arts","Business","Musical","Horror","Adventure","History","Comedy"]; 
 
+  @Select(ProfileState.profile) profile$! : Observable<ProfileDto | null>;
+
+  profile! : ProfileDto;
+
+  constructor(private modalController: ModalController,private formBuilder: FormBuilder, private store: Store) {
+      if(!this.profile){
+      this.store.dispatch(new SubscribeToProfile());
+      this.profile$.subscribe((profile) => {
+        if(profile){
+          console.log(profile);
+          this.profile = profile;
+        }
+      })
+    }
+  }
+
+  postForm = this.formBuilder.group({
+    title: ['', [ Validators.required, Validators.maxLength(100)]],
+    text: ['', Validators.maxLength(1000)],
+    community: ['', Validators.required],
+    category: [[]]
+  });
+
+
+  
+  onSubmit(){
+    console.log(this.postForm.value);
+  }
   closePopup() {
     this.modalController.dismiss();
   }
