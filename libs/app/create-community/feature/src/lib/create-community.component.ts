10d7291c388ba1ea92bc07618f9c41428b80f8fe
RoninBrookes <126901461+RoninBrookesTuks@users.noreply.github.com>
2023-06-12 08:59:02 +0200

    CreateCommunity Created

---
+++ libs/app/create-community/feature/src/lib/create-community.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'create-community',
+  templateUrl: './create-community.component.html',
+  styleUrls: ['./create-community.component.css']
+})
+export class CreateCommunityComponent {
+
+}
