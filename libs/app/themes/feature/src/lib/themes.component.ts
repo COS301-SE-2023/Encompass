2c033a8e68d7b85432a63a35b9ff3d1fb5e3b15d
Julianna-Venter <juliannaventer@gmail.com>
2023-06-12 17:21:55 +0200

    Added themes and settings pages

---
+++ libs/app/themes/feature/src/lib/themes.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'themes',
+  templateUrl: './themes.component.html',
+  styleUrls: ['./themes.component.css']
+})
+export class ThemesComponent {
+
+}
