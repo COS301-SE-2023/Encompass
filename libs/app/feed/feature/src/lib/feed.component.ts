9d160542a239b5bceb9b065b4f466ecfae87ada6
SameetKeshav <u21479373@tuks.co.za>
2023-06-24 18:28:47 +0200

    Add code

--- libs/app/feed/feature/src/lib/feed.component.ts
+++ libs/app/feed/feature/src/lib/feed.component.ts
@@ -1,4 +1,19 @@
 import { Component } from '@angular/core';
+import { HomeApi } from '@encompass/app/home-page/data-access';
+import { Select, Store } from '@ngxs/store';
+import { HomeState } from '@encompass/app/home-page/data-access';
+import { Observable } from 'rxjs';
+import { HomeDto } from '@encompass/api/home/data-access';
+import { Router } from '@angular/router';
+import { GetAllPosts, getHome } from '@encompass/app/home-page/util';
+import { Console } from 'console';
+import { ProfileState } from '@encompass/app/profile/data-access';
+import { ProfileDto } from '@encompass/api/profile/data-access';
+import { SubscribeToProfile } from '@encompass/app/profile/util';
+import { ModalController } from '@ionic/angular';
+import {CreatePostComponent} from '@encompass/app/create-post/feature';
+import { PostDto } from '@encompass/api/post/data-access';
+import {CreateCommunityComponent} from '@encompass/app/create-community/feature';
 
 @Component({
   selector: 'feed',
@@ -6,6 +21,71 @@ import { Component } from '@angular/core';
   styleUrls: ['./feed.component.scss']
 })
 export class FeedPage {
+
+  @Select(ProfileState.profile) profile$! : Observable<ProfileDto | null>;
+  @Select(HomeState.homePosts) homePosts$! : Observable<PostDto[] | null>;
+  
+  profile! : ProfileDto | null;
+  posts! : PostDto[] | null;
+  reports : boolean[] =[];
+  images : string[] = [];
+
+  constructor(private router: Router, private store: Store, private modalController: ModalController){
+    this.store.dispatch(new SubscribeToProfile())
+    this.profile$.subscribe((profile) => {
+      if(profile){
+        console.log(profile);
+        this.profile = profile;
+      }
+    });
+
+    this.store.dispatch(new GetAllPosts());
+    this.homePosts$.subscribe((posts) => {
+      if(posts){
+        console.log(posts);
+        console.log(posts.length)
+        this.posts = posts;
+        for(let i =0;i<posts.length;i++){
+              this.reports.push(false);
+            } 
+            console.log(this.reports);
+      }
+    })
+
+}
+
+async openPopup() {
+  const modal = await this.modalController.create({
+    component: CreatePostComponent,
+    cssClass: 'custom-modal', // Replace with the component or template for your popup
+    componentProps: {
+      // Add any input properties or data you want to pass to the popup component
+    }
+  });
+
+  return await modal.present();
+}
+
+async openPopup2() {
+  const modal = await this.modalController.create({
+    component: CreateCommunityComponent,
+    cssClass: 'custom-modal', // Replace with the component or template for your popup
+    componentProps: {
+      // Add any input properties or data you want to pass to the popup component
+    }
+  });
+
+  return await modal.present();
+}
+
+Report(n:number){
+  if(this.reports[n]==true){
+    this.reports[n]=false;
+  }else if(this.reports[n]==false){
+    this.reports[n]=true;
+  }
+ 
+}
 
   recChange(){
     const recBtn = document.getElementById('recommendedBtn');
