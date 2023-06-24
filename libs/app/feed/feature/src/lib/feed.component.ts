6fac1de1a589e01ff11f801b27569278f05add9c
Julianna-Venter <juliannaventer@gmail.com>
2023-06-24 16:26:04 +0200

    Filter buttons

--- libs/app/feed/feature/src/lib/feed.component.ts
+++ libs/app/feed/feature/src/lib/feed.component.ts
@@ -7,4 +7,42 @@ import { Component } from '@angular/core';
 })
 export class FeedPage {
 
+  recChange(){
+    const recBtn = document.getElementById('recommendedBtn');
+    const newBtn = document.getElementById('newBtn');
+    const popBtn = document.getElementById('popularBtn');
+
+    if (recBtn && newBtn && popBtn) {
+      recBtn.classList.add('active-button');
+      newBtn.classList.remove('active-button');
+      popBtn.classList.remove('active-button');
+    }
+      
+    
+  }
+
+  newChange(){
+    const recBtn = document.getElementById('recommendedBtn');
+    const newBtn = document.getElementById('newBtn');
+    const popBtn = document.getElementById('popularBtn');
+
+    if (recBtn && newBtn && popBtn) {
+      recBtn.classList.remove('active-button');
+      newBtn.classList.add('active-button');
+      popBtn.classList.remove('active-button');
+    }
+  }
+
+  popChange(){
+    const recBtn = document.getElementById('recommendedBtn');
+    const newBtn = document.getElementById('newBtn');
+    const popBtn = document.getElementById('popularBtn');
+
+    if (recBtn && newBtn && popBtn) {
+      recBtn.classList.remove('active-button');
+      newBtn.classList.remove('active-button');
+      popBtn.classList.add('active-button');
+    }
+  }
+
 }
