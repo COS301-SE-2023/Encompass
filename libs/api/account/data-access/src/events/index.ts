2c8d121844ba413b023294c03ff350f8d365d614
keamothapo <u21543462@tuks.co.za>
2023-06-25 23:28:41 +0200

    account events CS

--- libs/api/account/data-access/src/events/index.ts
+++ libs/api/account/data-access/src/events/index.ts
@@ -1,2 +1,12 @@
+/*
+FILENAME: index.ts
+
+AUTHOR: Sameet Keshav
+
+CREATION DATE: 27 May 2023
+
+DESCRIPTION: **
+*/
+
 export * from './account-created.event'
 export * from './account-created.handler'
