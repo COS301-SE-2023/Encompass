0d3a6ed1aa1741cc7ca87b879b66851dbae5cb9a
RoninBrookes <126901461+RoninBrookesTuks@users.noreply.github.com>
2023-06-25 04:24:20 +0200

    comments logic implemented

--- libs/app/comments/feature/src/lib/comments.component.ts
+++ libs/app/comments/feature/src/lib/comments.component.ts
@@ -6,5 +6,20 @@ import { Component } from '@angular/core';
   styleUrls: ['./comments.component.scss']
 })
 export class CommentsComponent {
+comment: boolean = false;
+reply : boolean = false;
+viewreply : boolean = false;
 
+
+AddComment(){
+  this.comment = !this.comment;
+}
+
+Reply(){
+  this.reply = !this.reply;
+}
+
+viewReplies(){
+  this.viewreply = !this.viewreply;
+}
 }
