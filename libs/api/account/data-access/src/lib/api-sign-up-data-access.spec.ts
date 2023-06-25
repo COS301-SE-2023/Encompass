0f12781eca722a7b0b95e567f4a104fe3d183274
keamothapo <u21543462@tuks.co.za>
2023-06-25 23:30:48 +0200

    account lib CS

--- libs/api/account/data-access/src/lib/api-sign-up-data-access.spec.ts
+++ libs/api/account/data-access/src/lib/api-sign-up-data-access.spec.ts
@@ -1,3 +1,13 @@
+/*
+FILENAME: api-sign-up-data-access.spec.ts
+
+AUTHOR: Sameet Keshav
+
+CREATION DATE: 27 May 2023
+
+DESCRIPTION: This file ensures that the api sign up data access works properly.
+*/
+
 import { apiSignUpDataAccess } from './api-sign-up-data-access';
 
 describe('apiSignUpDataAccess', () => {
