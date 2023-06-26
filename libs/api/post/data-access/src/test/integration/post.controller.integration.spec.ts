aa270d179110a4d404991f0555bf07b5b73000d3
Morgan90-B <u18103007@tuks.co.za>
2023-06-26 14:20:59 +0200

    a

--- libs/api/post/data-access/src/test/integration/post.controller.integration.spec.ts
+++ libs/api/post/data-access/src/test/integration/post.controller.integration.spec.ts
@@ -34,7 +34,72 @@ describe('PostController', () => {
         }); 
     });
 
+    /*describe('updatePost', () => {
+        it('should delete inserted Post and return id of deleted Post', async () => {
+            const { _id } = PostDtoStub();
+
+            await dbConnection.collection('Post').insertOne(PostDtoStub());
+            const response = await request(httpServer).delete(`/post/delete/${_id.toString()}`);
+            expect(response.status).toBe(200);
+
+            expect(response.text).toBe(_id.toString());
+        });
+        
+    });
+
+    describe('deletePost', () => {
+        it('should delete inserted Post and return id of deleted Post', async () => {
+            const { _id } = PostDtoStub();
+
+            await dbConnection.collection('Post').insertOne(PostDtoStub());
+            const response = await request(httpServer).delete(`/post/delete/${_id.toString()}`);
+            expect(response.status).toBe(200);
+
+            expect(response.text).toBe(_id.toString());
+        });
+        
+    });
+
+    describe('addReply', () => {
+        it('should return Post with added reply', async () => {
+            const { _id } = PostDtoStub();
+
+            await dbConnection.collection('post').insertOne(PostDtoStub());
+            const response = await request(httpServer)
+                .patch(`/post/add-reply/${_id.toString()}`)
+                .send(replyStub());
+
+            expect(response.status).toBe(200);
+            expect(response.body).not.toEqual(PostDtoStub());
+            expect(response.body.replies[0]).toMatchObject(replyStub());
+        }); 
+    });
+
+    describe('deleteReply', () => {
+        it('should return true when Post is found', async () => {
+            const replyId = PostWithReplyStub().replies[0].id;
+            const PostId = PostWithReplyStub()._id;
+
+            await dbConnection.collection('post').insertOne(PostWithReplyStub());
+            const response = await request(httpServer)
+                .delete(`/post/delete-reply/${PostId}/${replyId}`);
+            
+            expect(response.status).toBe(200);
+            expect(response.text).toBe(replyId);
+        });
+        
+    });
     
+    describe('getPostPosts', () => {
+        it('should return the Post inserted', async () => {
+            await dbConnection.collection('Post').insertOne(PostWithReplyStub());
+            const response = await request(httpServer).get(`/Post/get-post-Posts/${PostWithReplyStub()._id.toString()}`);
+            expect(response.status).toBe(200);
+            console.log(response);
+            //expect(response.body).toMatchObject(C);
+        });
+        
+    });*/
 
     afterEach(async () => {
         await dbConnection.collection('Post').deleteMany({});
