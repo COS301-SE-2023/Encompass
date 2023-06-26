246bcb3d23ed4dc4c5edea352d4927b6bb76acc4
SameetKeshav <u21479373@tuks.co.za>
2023-06-26 11:32:59 +0200

    Backend Integration for Profile Page

--- libs/api/comment/data-access/src/queries/get-by-username/get-by-username.handler.ts
+++ libs/api/comment/data-access/src/queries/get-by-username/get-by-username.handler.ts
@@ -1,12 +1,23 @@
 import { IQueryHandler, QueryHandler } from "@nestjs/cqrs";
 import { GetCommentsByUsernameQuery } from "./get-by-username.query";
-import { CommentDtoRepository } from "../../db/comment-dto.repository";
+import { CommentEntityRepository } from "../../db/comment-entity.repository";
 
 @QueryHandler(GetCommentsByUsernameQuery)
 export class GetCommentsByUsernameHandler implements IQueryHandler<GetCommentsByUsernameQuery>{
-  constructor(private commentDtoRepository: CommentDtoRepository){}
+  constructor(private commentEntityRepository: CommentEntityRepository){}
 
   async execute(query: GetCommentsByUsernameQuery){
-    return await this.commentDtoRepository.findByPostId(query.username);
+    const mainComments = await this.commentEntityRepository.findCommentsByUsername(query.username);
+    // const subComments = await this.commentEntityRepository.findSubCommentsByUsername(query.username);
+
+    // console.log(subComments);
+
+    const comments = mainComments;
+
+    // for(let i = 0; i < subComments.length; i++){
+    //   comments.findIndex(e => e === subComments[i]) === -1 ? comments.push(subComments[i]) : console.log(`User already exists}`);
+    // }
+
+    return comments;
   } 
 }
