175d3c7772f2dae5e98b18873ac93041f6af022f
Morgan90-B <u18103007@tuks.co.za>
2023-07-08 15:30:07 +0200

    placed profiles on n-dimension matrix

--- libs/api/community/data-access/src/queries/recommended/get-recommended-communities.handler.ts
+++ libs/api/community/data-access/src/queries/recommended/get-recommended-communities.handler.ts
@@ -44,20 +44,44 @@ export class GetRecommendedCommunitiesHandler implements IQueryHandler<GetRecomm
                 console.log("3NN algo");
                 //K means clustering profiles where K==3
                 
-                const profiles = []
-                const categories = ["Movies","Mathematics","Action","Science-Fiction","Drama","Romance","Anime","Books","Hospitality","Animation","Documentary","Physics","Geography","Series","Western","Mystery","Fantasy","Life Science","War","Information-Technology","Arts","Business","Musical","Horror","Adventure","History","Comedy"];
-                const following = [];
-                const events = [];
+                const profiles = [];
+                const categories: string[] = [];
+                //const categories = ["Movies","Mathematics","Action","Science-Fiction","Drama","Romance","Anime","Books","Hospitality","Animation","Documentary","Physics","Geography","Series","Western","Mystery","Fantasy","Life Science","War","Information-Technology","Arts","Business","Musical","Horror","Adventure","History","Comedy"];
+                const following: string[] = [];
+                const events: string[] = [];
                 const tempProfile = [];
 
-                //add events from all profiles to events array
+                //add categories from all profiles to categories array, categories array must have unique values
                 for(let i = 0; i < allProfiles?.data.length; i++){
-                    for(let j = 0; j < allProfiles?.data[i].events?.length; j++){
-                        events.push(allProfiles?.data[i].events[j]);
+                    for(let j = 0; j < allProfiles?.data[i].categories?.length; j++){
+                        if(categories.length < 1 || !categories.includes(allProfiles?.data[i].categories[j])){
+                            categories.push(allProfiles?.data[i].categories[j]);
+                        }
                     }
                 }
 
+                //add following from all profiles to following array, following array must have unique values
                 for(let i = 0; i < allProfiles?.data.length; i++){
+                    for(let j = 0; j < allProfiles?.data[i].following?.length; j++){
+                        if(following.length < 1 || !following.includes(allProfiles?.data[i].following[j])){
+                            following.push(allProfiles?.data[i].following[j]);
+                        }
+                    }
+                }
+                //console.log("following: ", following);
+
+                //add events from all profiles to events array, events array must have unique values
+                for(let i = 0; i < allProfiles?.data.length; i++){
+                    for(let j = 0; j < allProfiles?.data[i].events?.length; j++){
+                        if(events.length < 1 || !events.includes(allProfiles?.data[i].events[j])){
+                            events.push(allProfiles?.data[i].events[j]);
+                        }
+                    }
+                }
+                //console.log("events: ", events);
+
+                for(let i = 0; i < allProfiles?.data.length; i++){
+                    tempProfile.length = 0;
                     //for each category add 0 or one to tempProfile if profile has category
                     for(let j = 0; j < categories?.length; j++){
                         if(allProfiles?.data[i].categories?.includes(categories[j])){
@@ -67,35 +91,25 @@ export class GetRecommendedCommunitiesHandler implements IQueryHandler<GetRecomm
                         }
                     }
                     //for each profile add 0 or 1 to tempProfile if profile is following user
-                    for(let j = 0; j < allProfiles?.data[i].length; j++){
-                        let comparedUsers = 0;
-                        while(comparedUsers < userCount){
-                            if(allProfiles?.data[i].following[j] == allProfiles?.data[comparedUsers]._id){
-                                tempProfile.push(1);
-                            } else {
-                                tempProfile.push(0);
-                            }
-                            comparedUsers++;
+                    for(let j = 0; j < following.length; j++){
+                        if(allProfiles?.data[i].following?.includes(following[j])){
+                            tempProfile.push(1);
+                        } else {
+                            tempProfile.push(0);
                         }
                     }
-
                     //for each event add 0 or 1 to tempProfile if profile is attending event
                     for(let j = 0; j < events.length; j++){
-                        let comparedEvents = 0;
-                        while(comparedEvents < userCount){
-                            if(allProfiles?.data[i].events[j] == events[j]){
-                                tempProfile.push(1);
-                            } else if( allProfiles?.data[i].events[j] != events[j] || allProfiles?.data[i].events[j] == null ){
-                                tempProfile.push(0);
-                            }
-                            comparedEvents++;
+                        if(allProfiles?.data[i].events?.includes(events[j])){
+                            tempProfile.push(1);
+                        } else {
+                            tempProfile.push(0);
                         }
-                        console.log(allProfiles?.data[i].events[j]);
                     }
-                    
+                    console.log("tempProfile: ", tempProfile);
                     profiles.push(tempProfile);
                 }
-                console.log(profiles);
+                //console.log("profiles:" + profiles);
             }
             
         } catch (e) {
