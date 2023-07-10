c02ee1f224ad3c74a64a680bc68f448f984761af
Julianna-Venter <juliannaventer@gmail.com>
2023-07-10 22:53:04 +0200

    Theme page UI

--- libs/app/themes/feature/src/lib/themes.component.ts
+++ libs/app/themes/feature/src/lib/themes.component.ts
@@ -3,8 +3,13 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'themes',
   templateUrl: './themes.component.html',
-  styleUrls: ['./themes.component.css']
+  styleUrls: ['./themes.component.scss']
 })
 export class ThemesPage {
-
+  scrollImages() {
+    const container = document.querySelector('.images');
+    if (container) {
+      container.scrollLeft += container.clientWidth;
+    }
+  }
 }
