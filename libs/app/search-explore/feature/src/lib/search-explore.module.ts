a63509a2536b529674fc3ba550555cbd7bfbcb14
keamothapo <u21543462@tuks.co.za>
2023-07-13 04:07:17 +0200

    Files for searc explore

---
+++ libs/app/search-explore/feature/src/lib/search-explore.module.ts
@@ -0,0 +1,13 @@
+import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
+import { IonicModule } from '@ionic/angular';
+import { SearchExploreComponent } from './search-explore.component';
+//import { SearchExploreRouting } from './search-explore.routing';
+
+
+@NgModule({
+  imports: [CommonModule, IonicModule, SearchExploreRouting, ],
+  declarations: [SearchExploreComponent],
+  providers: [],
+})
+export class SearchExploreModule {}
