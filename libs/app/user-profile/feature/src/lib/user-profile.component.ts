725f3634ace437ec90eff28f9f1675e2993efdbe
RoninBrookes <126901461+RoninBrookesTuks@users.noreply.github.com>
2023-07-14 13:14:08 +0200

    User-Profile Updated

--- libs/app/user-profile/feature/src/lib/user-profile.component.ts
+++ libs/app/user-profile/feature/src/lib/user-profile.component.ts
@@ -8,6 +8,9 @@ import { UserProfileState } from '@encompass/app/user-profile/data-access';
 import { GetUserProfile, GetUserProfilePosts } from '@encompass/app/user-profile/util';
 import { Select, Store } from '@ngxs/store';
 import { Observable } from 'rxjs';
+import { UpdatePostRequest } from '@encompass/api/post/data-access';
+import { UpdatePost } from '@encompass/app/home-page/util';
+
 
 @Component({
   selector: 'user-profile',
@@ -22,6 +25,17 @@ export class UserProfile {
   userProfile!: ProfileDto | null;
   profile!: ProfileDto | null;
   userPosts!: PostDto[] | null;
+  seePosts=true;
+   seeComments=false;
+   shares : number[] = [];
+   sharing: boolean[] = [];
+   likes: number[] =[] ;
+   likedComments: boolean[] = [];
+   datesAdded : string[] = [];
+   comments  : number[] = [];
+
+
+
 
   constructor(private store: Store, private router: Router, private route: ActivatedRoute){
     const username = this.route.snapshot.paramMap.get('username');
@@ -41,6 +55,24 @@ export class UserProfile {
           if(userPosts){
             this.userPosts = userPosts
             console.log(this.userPosts)
+            for(let i =0;i<userPosts.length;i++){
+              this.likedComments.push(false);
+              this.sharing.push(false);
+
+              if(userPosts[i].dateAdded!=null&&userPosts[i].comments!=null
+                &&userPosts[i].shares!=null){
+                this.datesAdded.push(userPosts[i].dateAdded);
+                  this.comments.push(userPosts[i].comments);
+                  this.shares.push(userPosts[i].shares);
+            }
+
+              if(userPosts!=null&&userPosts[i].likes!=null){
+                this.likes.push(userPosts[i].likes?.length);
+                if(userPosts[i].likes?.includes(userPosts[i].username)){
+                  this.likedComments[i]=true;
+              }
+            }
+            }
           }
         })
       }
@@ -53,5 +85,92 @@ export class UserProfile {
         console.log(this.profile)
       }
     })
+  }
+
+  async Share(n:number, post: PostDto){
+    this.shares[n]++;
+    for(let i =0;i<this.sharing.length;i++){
+      this.sharing[i]=false;
+    }
+    this.sharing[n]=true;
+  
+    const obj = location.origin
+    if(obj == undefined){
+      return;
+    }
+  
+    const data : UpdatePostRequest = {
+      title: post.title,
+      text: post.text,
+      imageUrl: post.imageUrl,
+      communityImageUrl: post.communityImageUrl,
+      categories: post.categories,
+      likes: post.likes,
+      spoiler: post.spoiler,
+      ageRestricted: post.ageRestricted,
+      shares: post.shares + 1,
+      comments: post.comments,
+      reported: post.reported
+    }
+  
+    this.store.dispatch(new UpdatePost(post._id, data));
+  
+    const link : string = obj + '/app-comments-feature/' + post._id;
+  
+    await navigator.clipboard.writeText(link)
+  }
+
+  postChange(){
+    const PostBtn = document.getElementById('PostBtn');
+    const CommentsBtn = document.getElementById('CommentsBtn');
+    const eventBtn = document.getElementById('eventBtn');
+
+    if (PostBtn && CommentsBtn && eventBtn) {
+      PostBtn.classList.add('active-button');
+      CommentsBtn.classList.remove('active-button');
+      eventBtn.classList.remove('active-button');
+    }
+
+    this.seePosts=true;
+   this.seeComments=false;
+    
+  }
+
+  commChange(){
+    const PostBtn = document.getElementById('PostBtn');
+    const CommentsBtn = document.getElementById('CommentsBtn');
+    const eventBtn = document.getElementById('eventBtn');
+
+    if (PostBtn && CommentsBtn && eventBtn) {
+      PostBtn.classList.remove('active-button');
+      CommentsBtn.classList.add('active-button');
+      eventBtn.classList.remove('active-button');
+    }
+
+    this.seePosts=false;
+    this.seeComments=true;
+  }
+
+  eventChange(){
+    const PostBtn = document.getElementById('PostBtn');
+    const CommentsBtn = document.getElementById('CommentsBtn');
+    const eventBtn = document.getElementById('eventBtn');
+
+    if (PostBtn && CommentsBtn && eventBtn) {
+      PostBtn.classList.remove('active-button');
+      CommentsBtn.classList.remove('active-button');
+      eventBtn.classList.add('active-button');
+    }
+  }
+
+  GoToComments(postId : string){
+    this.router.navigate(['app-comments-feature/' + postId]);
+  }
+  presentingElement: any;
+  presentingElement2: any;
+
+  ngOnInit() {
+    this.presentingElement = document.querySelector('.ion-page');
+    this.presentingElement2 = document.querySelector('.ion-page');
   }
 }
