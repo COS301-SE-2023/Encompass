8d57bce138040a42f5e65d943faec167f7734b20
Morgan90-B <u18103007@tuks.co.za>
2023-07-14 13:30:21 +0200

    Media recommender module

--- libs/api/media-recommender/data-access/src/media-recommender.module.ts
+++ libs/api/media-recommender/data-access/src/media-recommender.module.ts
@@ -1,6 +1,23 @@
+import { CqrsModule } from "@nestjs/cqrs";
+import { BookSchema } from "./db/book.schema";
+import { MongooseModule, Schema, SchemaFactory } from "@nestjs/mongoose";
+import { Module } from "@nestjs/common";
+import { HttpModule } from "@nestjs/axios";
+
 @Module({
     imports: [
+        CqrsModule,
         MongooseModule.forFeature([
-            { name: .name, schema: MediaSchema },
+            { 
+                name: BookSchema.name, 
+                schema: SchemaFactory.createForClass(BookSchema),
+            },
+        ]),
+        HttpModule
+    ],
 
+    controllers: [],
+    providers: [],
 })
+
+export class MediaRecommenderModule {}
