cd87ea49e96f1704065402691461f33051e5d7df
Morgan90-B <u18103007@tuks.co.za>
2023-07-14 13:34:46 +0200

    Update book.ts

---
+++ libs/api/media-recommender/data-access/src/book.ts
@@ -0,0 +1,109 @@
+import { AggregateRoot } from "@nestjs/cqrs";
+
+export class Book extends AggregateRoot {
+    constructor(
+        public readonly _id: string,
+        public bookId: string,
+        public title: string,
+        public series: string,
+        public author: string,
+        public rating: number,
+        public description: string,
+        public language: string,
+        public isbn: string,
+        public genres: string[],
+        public characters: string[],
+        public bookFormat: string,
+        public edition: string,
+        public pages: number,
+        public publisher: string,
+        public publishDate: string,
+        public awards: string[],
+        public numRatings: number,
+        public ratingsByStars: string[],
+        public likedPercent: number,
+    ) {
+        super();
+    }
+
+    getId(): string {
+        return this._id;
+    }
+
+    getBookId(): string {
+        return this.bookId;
+    }
+
+    getTitle(): string {
+        return this.title;
+    }
+
+    getSeries(): string {
+        return this.series;
+    }
+
+    getAuthor(): string {
+        return this.author;
+    }
+
+    getRating(): number {
+        return this.rating;
+    }
+
+    getDescription(): string {
+        return this.description;
+    }
+
+    getLanguage(): string {
+        return this.language;
+    }
+
+
+    getIsbn(): string {
+        return this.isbn;
+    }
+
+    getGenres(): string[] {
+        return this.genres;
+    }
+
+    getCharacters(): string[] {
+        return this.characters;
+    }
+
+    getBookFormat(): string {
+        return this.bookFormat;
+    }
+
+    getEdition(): string {
+        return this.edition;
+    }
+
+    getPages(): number {
+        return this.pages;
+    }
+
+    getPublisher(): string {
+        return this.publisher;
+    }
+
+    getPublishDate(): string {
+        return this.publishDate;
+    }
+
+    getAwards(): string[] {
+        return this.awards;
+    }
+
+    getNumRatings(): number {
+        return this.numRatings;
+    }
+
+    getRatingsByStars(): string[] {
+        return this.ratingsByStars;
+    }
+
+    getLikedPercent(): number {
+        return this.likedPercent;
+    }
+}
