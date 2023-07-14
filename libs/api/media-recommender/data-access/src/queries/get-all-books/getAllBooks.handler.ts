78dccfc2f3ad0d979b4027495229cf3d225355f6
Morgan90-B <u18103007@tuks.co.za>
2023-07-14 13:48:31 +0200

    Update getAllBooks.handler.ts

--- libs/api/media-recommender/data-access/src/queries/get-all-books/getAllBooks.handler.ts
+++ libs/api/media-recommender/data-access/src/queries/get-all-books/getAllBooks.handler.ts
@@ -1,5 +1,12 @@
-import { QueryHandler } from "@nestjs/cqrs";
+import { IQueryHandler, QueryHandler } from "@nestjs/cqrs";
+import { GetAllBooksQuery } from "./getAllBooks.query";
+import { BookDtoRepository } from "../../db/book-dto.repository";
 
 @QueryHandler(GetAllBooksQuery)
 export class GetAllBooksHandler implements IQueryHandler<GetAllBooksQuery> {
-    constructor(private readonly 
+    constructor(private readonly bookDtoRepository: BookDtoRepository) {}
+
+    async execute() {
+        return await this.bookDtoRepository.findAll();
+    }
+}
