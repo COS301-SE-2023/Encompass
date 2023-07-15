2b7206ad8a9e5c08dbb21b9efde294abcb8450f8
Morgan90-B <u18103007@tuks.co.za>
2023-07-15 19:12:07 +0200

    arrayEqualityCheck

--- libs/api/media-recommender/data-access/src/queries/get-all-books/getRecommendedBooks.handler.ts
+++ libs/api/media-recommender/data-access/src/queries/get-all-books/getRecommendedBooks.handler.ts
@@ -135,6 +135,23 @@ export class GetRecommendedBooksHandler implements IQueryHandler<GetRecommendedB
             return Math.sqrt(distance);
         }
 
+        function arraysAreEqual(array1: number[][], array2: number[][]) {
+            if(array1.length != array2.length){
+                return false;
+            }
+            for(let i = 0; i < array1.length; i++){
+                if(array1[i].length != array2[i].length){
+                    return false;
+                }
+                for(let j = 0; j < array1[i].length; j++){
+                    if(array1[i][j] != array2[i][j]){
+                        return false;
+                    }
+                }
+            }
+            return true;
+        }
+
         
 
         return await this.bookEntityRepository.findSome();
