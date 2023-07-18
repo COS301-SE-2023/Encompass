6394d95cc6ad0c8a8bffa93131ab9581e6345a2e
Morgan90-B <u18103007@tuks.co.za>
2023-07-18 17:31:30 +0200

    Update getAllPosts.handler.ts

--- libs/api/post/data-access/src/queries/getAllPosts.handler.ts
+++ libs/api/post/data-access/src/queries/getAllPosts.handler.ts
@@ -3,6 +3,9 @@ import { GetAllPostsQuery } from "./getAllPosts.query";
 import { PostDtoRepository } from "../db/post-dto.repository";
 import { PostSchema } from "../db/post.schema";
 import { HttpService } from "@nestjs/axios";
+import { PostDto } from "../post.dto";
+import { ObjectId, Document } from "mongoose";
+import mongoose from "mongoose";
 
 @QueryHandler(GetAllPostsQuery)
 export class GetAllPostsHandler implements IQueryHandler<GetAllPostsQuery> {
@@ -12,21 +15,37 @@ export class GetAllPostsHandler implements IQueryHandler<GetAllPostsQuery> {
   ) {}
 
   async execute({ username }: GetAllPostsQuery) {
+    //complex types
+    type postType = (Document<unknown, object, PostSchema> & Omit<PostSchema & Required<{
+        _id: mongoose.mongo.BSON.ObjectId;
+    }>, never>); 
     //AI
     console.log("Get recommended posts..........");
     const url = process.env["BASE_URL"];
     try {
-      const postsNotByUser = await this.postDtoRepository.getPostsNotByUser(username);
-      //const currentUserProfilePromise = this.httpService.get(url + "api/profile/get/" + userId).toPromise();
+      const postsNotByUserPromise = this.postDtoRepository.getPostsNotByUser(username);
+      const currentUserProfilePromise = this.httpService.get(url + "/api/profile/get-user/" + username).toPromise();
+      const [postsNotByUser, currentUserProfile] = await Promise.all([postsNotByUserPromise, currentUserProfilePromise]);
+      const userId = currentUserProfile?.data._id;
+
       const postCount = postsNotByUser.length;
-      if (postCount < 10) {
+      if (postCount < 1) {
         const recommendedPosts = orderbyPopularity(postsNotByUser);
         return recommendedPosts;
         //console.log(recommendedPosts);
       } else {
-        console.log("wtf");
-        const K = defineK(postCount);
-
+        addUserToPosts(postsNotByUser, currentUserProfile?.data);
+        //K-means clustering
+        const clusters = kmeans(postsNotByUser);
+        //get closest cluster to current user profile
+        const recommendedCluster = getClusterOfCurrentProfile(clusters, userId);
+        //remove current user profile from cluster
+        const recommendedPosts = recommendedCluster[0].clusterPosts.filter(post => post.postId !== userId);
+        //get recommended Posts from allPosts by _id
+        const recommendedPostsFromAllPosts = postsNotByUser.filter(post => recommendedPosts.some(recommendedPost => recommendedPost.postId === post._id.toString()));
+        //order recommended posts by popularity
+        const orderedRecommendedPosts = orderbyPopularity(recommendedPostsFromAllPosts);
+        return orderedRecommendedPosts;
       }
 
       //append liked posts at the end
@@ -34,36 +53,7 @@ export class GetAllPostsHandler implements IQueryHandler<GetAllPostsQuery> {
       console.log(error);
     }
 
-    function defineK(postCount: number) {
-      const k = Math.sqrt(postCount);
-      return Math.floor(k);
-    }
-
-    function orderbyPopularity(postsNotByUser: PostSchema[]) {
-
-      interface PostSchemaWithPopularity extends PostSchema {
-        popularity: number;
-      }
-
-
-
-      const postsWithPopularity: PostSchemaWithPopularity[] = postsNotByUser.map(post => {
-        const { shares, comments, likes } = post;
-        const shareWeight = 1;
-        const commentWeight = 1.2;
-        const likeWeight = 0.8;
-        const popularity = shares * shareWeight + comments * commentWeight + likes.length * likeWeight;
     
-        return { ...post, popularity };
-      });
-    
-      postsWithPopularity.sort((a, b) => b.popularity - a.popularity);
-    
-      return postsWithPopularity.map(post => {
-        const { popularity, ...rest } = post;
-        return rest; // Remove the temporary 'popularity' property from the post objects
-      });
-    }
 
     return await this.postDtoRepository.findAll();
   }
