33dabed7c6ace0bc4bc5dc77383ed67868568d2a
SameetKeshav <u21479373@tuks.co.za>
2023-07-25 07:46:02 +0200

    Update main.ts

--- apps/encompass/src/main.ts
+++ apps/encompass/src/main.ts
@@ -3,14 +3,14 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 // import { AppModule } from './app/app.module';
 import { CoreModule } from '@encompass/app/core/feature';
 
-const ENVIRONMENT = process.env['NX_ENVIRONMENT'] || 'development';
+// const ENVIRONMENT = process.env['NX_ENVIRONMENT'] || 'development';
 // platformBrowserDynamic()
 //   .bootstrapModule(AppModule)
 //   .catch((err) => console.error(err));
 
-if (ENVIRONMENT === 'production') {
-  enableProdMode();
-}
+// if (ENVIRONMENT === 'production') {
+//   enableProdMode();
+// }
 
 platformBrowserDynamic()
   .bootstrapModule(CoreModule)
