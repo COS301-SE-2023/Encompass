92cb43b2aaae0c3d14176e924914ec55a888fc44
SameetKeshav <u21479373@tuks.co.za>
2023-07-27 11:35:51 +0200

    Update core.module.ts

--- libs/app/core/feature/src/core.module.ts
+++ libs/app/core/feature/src/core.module.ts
@@ -24,7 +24,7 @@ import { ThemesModule } from '@encompass/app/themes/data-access';
 import { SignUpCommunitiesModule } from '@encompass/app/sign-up-interior2/data-access';
 
 const config: SocketIoConfig = {
-  url: process.env['BASE_URL'] || 'http://localhost:3000',
+  url: 'https://encompass-hosting.onrender.com',
   options: {}
 };
 @NgModule({
