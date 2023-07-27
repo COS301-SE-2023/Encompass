7c818b3968c7cf5fc5b066708b473a8f8b54f4ba
Morgan90-B <u18103007@tuks.co.za>
2023-07-27 10:51:36 +0200

    Update get-users-by-keyword.handler.ts

--- libs/api/profile/data-access/src/queries/search-profiles/get-users-by-keyword.handler.ts
+++ libs/api/profile/data-access/src/queries/search-profiles/get-users-by-keyword.handler.ts
@@ -1,6 +1,6 @@
 import { IQueryHandler, QueryHandler } from "@nestjs/cqrs";
 import { GetUsersByKeywordQuery } from "./get-users-by-keyword.query";
-import { ProfileEntityRepository } from "libs/api/profile/data-access/src/db/profile-entity.repository";
+import { ProfileEntityRepository } from "../../db/profile-entity.repository";
 
 @QueryHandler(GetUsersByKeywordQuery)
 export class GetUsersByKeywordHandler implements IQueryHandler<GetUsersByKeywordQuery> {
