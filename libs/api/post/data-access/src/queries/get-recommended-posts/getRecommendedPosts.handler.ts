7c6edc66ff944a3e4c35b5c1ccd66e019f118158
Morgan90-B <u18103007@tuks.co.za>
2023-07-28 09:08:30 +0200

    Update getRecommendedPosts.handler.ts

--- libs/api/post/data-access/src/queries/get-recommended-posts/getRecommendedPosts.handler.ts
+++ libs/api/post/data-access/src/queries/get-recommended-posts/getRecommendedPosts.handler.ts
@@ -22,30 +22,38 @@ export class GetRecommendedPostsHandler implements IQueryHandler<GetRecommendedP
 
     //AI
     const url = process.env["BASE_URL"];
-    console.log("url");
-    const allPostsPromise = this.postDtoRepository.findAll();
-    const recommendedUsersPromise = this.httpService.get(`${url}/api/profile/get-recommended/${id}`).toPromise();
-    const currentUserPromise = this.httpService.get(`${url}/api/profile/get/${id}`).toPromise();
-    const [allPosts, recommendedUsers, currentUser] = await Promise.all([allPostsPromise, recommendedUsersPromise, currentUserPromise]);
-    const recommendedProfiles = recommendedUsers?.data;
-    const currentUserData = currentUser?.data;
 
-    const profileNames = recommendedProfiles.map((profile: { username: string; }) => profile.username);
-    //recommendedPosts are all posts with the username or likes by the username, NB: likes is an array of usernames
-    const recommendedPosts = allPosts.filter((post: { username: string; likes: string[]; }) => profileNames.includes(post.username) || post.likes.some((like: string) => profileNames.includes(like)));
-    
-    const currentUserName = currentUserData.username;
-    const postsToRemove = recommendedPosts.filter((post: { username: string; likes: string[]; }) => post.username === currentUserName || post.likes.includes(currentUserName));
-    //remove all posts by the current user or liked by the current user from recommendedPosts and append them at the end
-    postsToRemove.forEach((post: postType) => {
-        const index = recommendedPosts.indexOf(post);
-        recommendedPosts.splice(index, 1);
-        recommendedPosts.push(post);
-    });
-    //add the rest of the posts to the bottom
-    const postsToAdd = allPosts.filter((post: postType) => !recommendedPosts.includes(post));
-    recommendedPosts.push(...postsToAdd);
+    try {
+      const allPostsPromise = this.postDtoRepository.findAll();
+      const recommendedUsersPromise = this.httpService.get(`${url}/api/profile/get-recommended/${id}`).toPromise();
+      const currentUserPromise = this.httpService.get(`${url}/api/profile/get/${id}`).toPromise();
+      const [allPosts, recommendedUsers, currentUser] = await Promise.all([allPostsPromise, recommendedUsersPromise, currentUserPromise]);
+      const recommendedProfiles = recommendedUsers?.data;
+      const currentUserData = currentUser?.data;
 
-    return recommendedPosts;
+      const profileNames = recommendedProfiles.map((profile: { username: string; }) => profile.username);
+      
+      //recommendedPosts are all posts with the username or likes by the username, NB: likes is an array of usernames
+      const recommendedPosts = allPosts.filter((post: { username: string; likes: string[]; }) => profileNames.includes(post.username) || post.likes.some((like: string) => profileNames.includes(like)));
+      
+      const currentUserName = currentUserData.username;
+      const postsToRemove = recommendedPosts.filter((post: { username: string; likes: string[]; }) => post.username === currentUserName || post.likes.includes(currentUserName));
+      
+      //remove all posts by the current user or liked by the current user from recommendedPosts and append them at the end
+      postsToRemove.forEach((post: postType) => {
+          const index = recommendedPosts.indexOf(post);
+          recommendedPosts.splice(index, 1);
+          recommendedPosts.push(post);
+      });
+
+      //add the rest of the posts to the bottom
+      const postsToAdd = allPosts.filter((post: postType) => !recommendedPosts.includes(post));
+      recommendedPosts.push(...postsToAdd);
+      return recommendedPosts;
+
+    } catch (error) {
+      console.log(error);
+      return [];
+    }
   }
 }
