d434a6335207958667f502d3da6c222d0809aeb1
Morgan90-B <u18103007@tuks.co.za>
2023-07-28 09:01:50 +0200

    posts AI done

--- libs/api/post/data-access/src/queries/get-recommended-posts/getRecommendedPosts.handler.ts
+++ libs/api/post/data-access/src/queries/get-recommended-posts/getRecommendedPosts.handler.ts
@@ -1,7 +1,11 @@
 import { IQueryHandler, QueryHandler } from "@nestjs/cqrs";
 import { GetRecommendedPostsQuery } from "./getRecommendedPosts.query";
 import { PostDtoRepository } from "../../db/post-dto.repository";
+import { PostSchema } from "../../db/post.schema";
+import { PostDto } from "../../post.dto";
+import { ObjectId, Document } from "mongoose";
 import { HttpService } from "@nestjs/axios";
+import mongoose from "mongoose";
 
 @QueryHandler(GetRecommendedPostsQuery)
 export class GetRecommendedPostsHandler implements IQueryHandler<GetRecommendedPostsQuery> {
@@ -11,6 +15,12 @@ export class GetRecommendedPostsHandler implements IQueryHandler<GetRecommendedP
   ) {}
 
   async execute( { id }: GetRecommendedPostsQuery ) {
+    //complex types
+    type postType = (Document<unknown, object, PostSchema> & Omit<PostSchema & Required<{
+      _id: mongoose.mongo.BSON.ObjectId;
+    }>, never>); 
+
+    //AI
     const url = process.env["BASE_URL"];
     console.log("url");
     const allPostsPromise = this.postDtoRepository.findAll();
@@ -19,32 +29,23 @@ export class GetRecommendedPostsHandler implements IQueryHandler<GetRecommendedP
     const [allPosts, recommendedUsers, currentUser] = await Promise.all([allPostsPromise, recommendedUsersPromise, currentUserPromise]);
     const recommendedProfiles = recommendedUsers?.data;
     const currentUserData = currentUser?.data;
-    console.log("recommendedProfiles");
-    console.log(recommendedProfiles.length);
 
     const profileNames = recommendedProfiles.map((profile: { username: string; }) => profile.username);
     //recommendedPosts are all posts with the username or likes by the username, NB: likes is an array of usernames
     const recommendedPosts = allPosts.filter((post: { username: string; likes: string[]; }) => profileNames.includes(post.username) || post.likes.some((like: string) => profileNames.includes(like)));
-    console.log("recommendedPosts");
-    console.log(recommendedPosts);
     
     const currentUserName = currentUserData.username;
-    console.log("currentUserName");
-    console.log(currentUserName);
     const postsToRemove = recommendedPosts.filter((post: { username: string; likes: string[]; }) => post.username === currentUserName || post.likes.includes(currentUserName));
-    console.log("recommended posts:");
-    console.log(recommendedPosts);
-    console.log("posts to remove:");
-    console.log(postsToRemove);
     //remove all posts by the current user or liked by the current user from recommendedPosts and append them at the end
-    postsToRemove.forEach((post: { username: string; likes: string[]; }) => {
+    postsToRemove.forEach((post: postType) => {
         const index = recommendedPosts.indexOf(post);
         recommendedPosts.splice(index, 1);
         recommendedPosts.push(post);
     });
-
     //add the rest of the posts to the bottom
+    const postsToAdd = allPosts.filter((post: postType) => !recommendedPosts.includes(post));
+    recommendedPosts.push(...postsToAdd);
 
-    return await this.postDtoRepository.findAll();
+    return recommendedPosts;
   }
 }
