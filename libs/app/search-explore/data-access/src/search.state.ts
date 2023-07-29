ba8234b4063278b438f500d1b39c1f1b99f88ca3
Morgan90-B <u18103007@tuks.co.za>
2023-07-29 17:57:42 +0200

    state

---
+++ libs/app/search-explore/data-access/src/search.state.ts
@@ -0,0 +1,63 @@
+import { CommunityDto } from "@encompass/api/community/data-access"
+import { PostDto } from "@encompass/api/post/data-access"
+import { ProfileDto } from "@encompass/api/profile/data-access"
+import { State } from "@ngxs/store"
+
+export interface SearchModel{
+    SearchPostsForm: {
+        model: {
+            posts: PostDto[] | null
+        }
+    }
+}
+
+export interface SearchProfilesModel{
+    SearchProfilesForm: {
+        model: {
+            profiles: ProfileDto[] | null
+        }
+    }
+}
+
+export interface SearchCommunitiesModel{
+    SearchCommunitiesForm: {
+        model: {
+            communities: CommunityDto[] | null
+        }
+    }
+}
+
+@State<SearchModel>({
+    name: 'searchPosts',
+    defaults: {
+        SearchPostsForm: {
+            model: {
+                posts: null
+            }
+        }
+    }
+})
+
+@State<SearchProfilesModel>({
+    name: 'searchProfiles',
+    defaults: {
+        SearchProfilesForm: {
+            model: {
+                profiles: null
+            }
+        }
+    }
+})
+
+@State<SearchCommunitiesModel>({
+    name: 'searchCommunities',
+    defaults: {
+        SearchCommunitiesForm: {
+            model: {
+                communities: null
+            }
+        }
+    }
+})
+
+
