24fc381bae178323ede632af7c9cdb0fe676dcff
RoninBrookes <126901461+RoninBrookesTuks@users.noreply.github.com>
2023-08-13 13:00:40 +0200

    event.ts implemented

---
+++ libs/api/event/data-access/src/event.ts
@@ -0,0 +1,66 @@
+import { AggregateRoot } from "@nestjs/cqrs";
+
+export class Event extends AggregateRoot{
+  constructor( 
+    public readonly _id: string ,
+    public name: string | null,
+    public host: string | null,
+    public community: string | null,
+    public description: string | null,
+    public startDate: Date | null,
+    public endDate: Date | null,
+    public members: string[] | null,
+    public quiz: string[] | null,
+    public memo: string[] | null,
+    public prompt: string[] | null,
+  ){
+    super();
+  }
+  
+  getId(): string {
+    return this._id;
+  }
+
+  getName(): string | null{
+    return this.name;
+  }
+
+  getHost(): string | null{
+    return this.host;
+  }
+
+  getCommunity(): string | null{
+    return this.community;
+  }
+
+  getDescription(): string | null{
+    return this.description;
+  }
+
+  getStartDate(): Date | null{
+    return this.startDate;
+  }
+
+  getEndDate(): Date | null{
+    return this.endDate;
+  }
+
+  getMembers(): string [] | null{
+    return this.members;
+  }
+
+  getQuiz(): string [] | null{
+    return this.quiz;
+  }
+
+  getMemo(): string [] | null{
+    return this.memo;
+  }
+
+  getPrompt(): string [] | null{
+    return this.prompt;
+  }
+  
+
+  
+}
