b186286d8eb6ae1175c3d9b8f493392c5356b67d
RoninBrookes <126901461+RoninBrookesTuks@users.noreply.github.com>
2023-08-13 13:05:52 +0200

    event.factory.ts implemented

---
+++ libs/api/event/data-access/src/event.factory.ts
@@ -0,0 +1,45 @@
+import { EntityFactory } from "@encompass/api/database/data-access";
+import { Injectable } from "@nestjs/common";
+import { EventEntityRepository } from "./db/event-entity.repository";
+import { ObjectId } from "mongodb";
+ import { EventCreatedEvent } from "./events";
+import { Event } from "./event";
+
+@Injectable()
+export class EventFactory implements EntityFactory<Event>{
+  constructor(
+    private readonly eventEntityRepository: EventEntityRepository,
+  ){}
+
+  async create(
+    _id: string,
+    name: string | null,
+    host: string | null,
+    community: string | null,
+    description: string | null,
+    startDate: Date | null,
+    endDate: Date | null,
+    members: string[] | null,
+    quiz: string[] | null,
+    memo: string[] | null,
+    prompt: string[] | null,
+  ) : Promise<Event>{
+    const event = new Event(
+      new ObjectId(_id).toHexString(),
+       name,
+        host,
+        community,
+        description,
+        startDate,
+        endDate,
+        members,
+        quiz,
+        memo,
+        prompt,
+      
+    );
+    await this.eventEntityRepository.create(event);
+    event.apply(new EventCreatedEvent(event.getId()))
+    return event;
+  }
+}
