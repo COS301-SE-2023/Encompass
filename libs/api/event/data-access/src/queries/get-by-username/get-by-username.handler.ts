f4e1fac2de4fd2e47f916073d13e4db44d8cff6c
RoninBrookes <126901461+RoninBrookesTuks@users.noreply.github.com>
2023-08-13 13:08:50 +0200

    get by username query implemented

---
+++ libs/api/event/data-access/src/queries/get-by-username/get-by-username.handler.ts
@@ -0,0 +1,12 @@
+import { IQueryHandler, QueryHandler } from "@nestjs/cqrs";
+import { GetByUsernameQuery } from "./get-by-username.query";
+import { EventDtoRepository } from "../../db/event-dto.repository";
+
+@QueryHandler(GetByUsernameQuery)
+export class GetByUsernameHandler implements IQueryHandler<GetByUsernameQuery>{
+  constructor(private readonly eventDtoRepository: EventDtoRepository){}
+
+  async execute(query: GetByUsernameQuery){
+    return await this.eventDtoRepository.getEventsByUsername(query.userId);
+  }
+}
