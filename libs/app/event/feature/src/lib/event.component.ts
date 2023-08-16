202e7a7361dc06f168eafc5836d01be3aecacf61
RoninBrookes <126901461+RoninBrookesTuks@users.noreply.github.com>
2023-08-16 07:06:00 +0200

    Event Page UI implemented

---
+++ libs/app/event/feature/src/lib/event.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'event',
+  templateUrl: './event.component.html',
+  styleUrls: ['./event.component.css']
+})
+export class EventPage {
+
+}
