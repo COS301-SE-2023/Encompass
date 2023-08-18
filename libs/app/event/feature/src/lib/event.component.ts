56e30206fea3ae69262bac286eecacff1b6d41c6
RoninBrookes <126901461+RoninBrookesTuks@users.noreply.github.com>
2023-08-18 22:21:57 +0200

    Event MOCK data implemented

--- libs/app/event/feature/src/lib/event.component.ts
+++ libs/app/event/feature/src/lib/event.component.ts
@@ -1,10 +1,33 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+
 
 @Component({
   selector: 'event',
   templateUrl: './event.component.html',
-  styleUrls: ['./event.component.css']
+  styleUrls: ['./event.component.scss']
 })
 export class EventPage {
 
+  constructor(private formBuilder: FormBuilder){
+    
+   
+
+    
+  }
+
+  categories = ["Action","Comedy","Fantasy"];
+  events = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17];
+  isValid = false;
+  inputValue! : string;
+
+  sendMessage(){
+    return;
+  }
+  checkInput(){
+    return;
+  }
+  messageForm = this.formBuilder.group({
+    messageInput: ['', Validators.maxLength(150)],
+  });
 }
