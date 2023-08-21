5eaa7e7970739d98135768a738443d7ce5d41c97
RoninBrookes <126901461+RoninBrookesTuks@users.noreply.github.com>
2023-08-21 15:41:50 +0200

    event code clean up

--- libs/app/event/feature/src/lib/event.component.ts
+++ libs/app/event/feature/src/lib/event.component.ts
@@ -1,30 +1,23 @@
 import { Component } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
-
 @Component({
   selector: 'event',
   templateUrl: './event.component.html',
-  styleUrls: ['./event.component.scss']
+  styleUrls: ['./event.component.scss'],
 })
 export class EventPage {
+  constructor(private formBuilder: FormBuilder) {}
 
-  constructor(private formBuilder: FormBuilder){
-    
-   
-
-    
-  }
-
-  categories = ["Action","Comedy","Fantasy"];
-  events = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17];
+  categories = ['Action', 'Comedy', 'Fantasy'];
+  events = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17];
   isValid = false;
-  inputValue! : string;
+  inputValue!: string;
 
-  sendMessage(){
+  sendMessage() {
     return;
   }
-  checkInput(){
+  checkInput() {
     return;
   }
   messageForm = this.formBuilder.group({
