b2f7d6c9e1424e78e112d079e409e55519100695
SameetKeshav <u21479373@tuks.co.za>
2023-08-21 07:35:09 +0200

    Added the backend commands to automate the user events system

--- libs/api/profile/data-access/src/commands/add-event/add-event.handler.ts
+++ libs/api/profile/data-access/src/commands/add-event/add-event.handler.ts
@@ -1,15 +1,19 @@
 import { CommandHandler, EventPublisher, ICommandHandler } from "@nestjs/cqrs";
 import { AddEventCommand } from "./add-event.command";
 import { ProfileEntityRepository } from "../../db/profile-entity.repository";
+import { HttpService } from "@nestjs/axios";
 
 @CommandHandler(AddEventCommand)
 export class AddEventHandler implements ICommandHandler<AddEventCommand>{
   constructor(
     private readonly eventPublisher: EventPublisher,
     private readonly profileEntityRepository: ProfileEntityRepository,
+    private readonly httpService: HttpService
   ){}
 
   async execute({ username, eventId }: AddEventCommand){
+    const url = process.env['BASE_URL'];
+
     const profile = this.eventPublisher.mergeObjectContext(
       await this.profileEntityRepository.findOneByUsername(username)
     );
@@ -17,6 +21,14 @@ export class AddEventHandler implements ICommandHandler<AddEventCommand>{
     profile.addEvent(eventId);
     this.profileEntityRepository.findOneAndReplaceById(profile._id, profile);
     profile.commit();
+
+    try{
+      this.httpService.patch(`${url}/api/user-events/add-event/${profile._id}/${eventId}`).toPromise()
+    }
+
+    catch(error){
+      console.log(error)
+    }
 
     return profile;
   }
