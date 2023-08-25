3b29d54487c48dbe962f97c67c7e0c9ffaa4b54d
Morgan90-B <u18103007@tuks.co.za>
2023-08-25 12:58:29 +0200

    home tests

---
+++ apps/api-e2e/src/api/home.spec.ts
@@ -0,0 +1,67 @@
+import { INestApplication } from "@nestjs/common";
+import { Test, TestingModule } from "@nestjs/testing";
+import mongoose, { Connection, Schema } from "mongoose";
+import { AppModule } from "./app.module";
+import { MongooseModule } from "@nestjs/mongoose";
+
+export interface Home extends Document {
+    readonly name: string;
+}
+
+export const HomeSchema = new Schema<Home>(
+    {
+        name: { type: String, required: true },
+    },
+)
+
+
+const dbUrl = process.env['NX_MONGO_DB_TEST']; 
+
+const connectToDatabase = async () => {
+  try {
+    // Connect to the MongoDB database
+    await mongoose.connect(dbUrl);
+
+    console.log('Connected to the database!');
+
+    // Get the Mongoose connection
+    const dbConnection = mongoose.connection;
+
+    // Optional: You can add event listeners to handle connection events
+    dbConnection.on('error', (error) => {
+      console.error('Database connection error:', error);
+    });
+
+    dbConnection.on('disconnected', () => {
+      console.log('Disconnected from the database');
+    });
+
+    // Return the Mongoose connection so you can use it in other parts of your application
+    return dbConnection;
+  } catch (error) {
+    console.error('Error connecting to the database:', error);
+  }
+};
+
+
+describe('ChatController (Integration with MongoDB)', () => {
+  let app: INestApplication; 
+  let dbConnection: Connection;
+
+  const setupTestApp = async () => {
+    const moduleFixture: TestingModule = await Test.createTestingModule({
+      imports: [
+        AppModule,
+        MongooseModule.forFeature([{ name: 'home', schema: ChatListSchema }])
+      ],
+    }).compile();
+
+    app = moduleFixture.createNestApplication();
+    await app.init();
+
+    dbConnection = await connectToDatabase();
+  };
+
+  afterEach(async () => {
+    await dbConnection.collection('home').deleteMany({});
+  });
