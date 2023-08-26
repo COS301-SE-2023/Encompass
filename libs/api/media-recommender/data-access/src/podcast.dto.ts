4ddb5fdda134e26fd3a4f686f1588e026fe2711d
Morgan90-B <u18103007@tuks.co.za>
2023-08-26 20:10:27 +0200

    Create podcast.dto.ts

---
+++ libs/api/media-recommender/data-access/src/podcast.dto.ts
@@ -0,0 +1,10 @@
+export class PodcastDto {
+    readonly _id!: string;
+    readonly title!: string;
+    readonly image!: string;
+    readonly description!: string;
+    readonly language!: string;
+    readonly categories!: string;
+    readonly author!: string;
+    readonly website!: string;
+}
