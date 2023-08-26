d929ca93511ea9df092083ebc35964bf73bbc664
Morgan90-B <u18103007@tuks.co.za>
2023-08-26 20:20:35 +0200

    THIS WILL MESS UP ALL CODE
    ai implementation of other media was deprecated

---
+++ libs/api/media-recommender/data-access/src/queries/get-recommended-podcasts/getRecommendedPodcasts.handler.ts
@@ -0,0 +1,7 @@
+import { IQueryHandler, QueryHandler } from "@nestjs/cqrs";
+import { GetRecommendedPodcastsQuery } from "./getRecommendedPodcasts.query";
+
+@QueryHandler(GetRecommendedPodcastsQuery)
+export class GetRecommendedPodcastsHandler implements IQueryHandler<GetRecommendedPodcastsQuery> {
+    
+}
