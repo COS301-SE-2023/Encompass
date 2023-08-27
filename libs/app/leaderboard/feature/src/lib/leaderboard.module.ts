5ddad569e9e3f92defe1f3ad9fc82a1099f8033f
RoninBrookes <126901461+RoninBrookesTuks@users.noreply.github.com>
2023-08-27 19:15:20 +0200

    leaderboard module update

---
+++ libs/app/leaderboard/feature/src/lib/leaderboard.module.ts
@@ -0,0 +1,11 @@
+import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
+import { IonicModule } from '@ionic/angular';
+import { LeaderboardComponent } from './leaderboard.component';
+
+@NgModule({
+  imports: [CommonModule, IonicModule],
+  declarations: [LeaderboardComponent],
+  providers: [],
+})
+export class LeaderboardModule {}
