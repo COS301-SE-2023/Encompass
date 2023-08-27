6a2749318708dfc5f202f5d883a1fafb3fd70337
RoninBrookes <126901461+RoninBrookesTuks@users.noreply.github.com>
2023-08-27 19:10:32 +0200

    Update event.component.ts

--- libs/app/event/feature/src/lib/event.component.ts
+++ libs/app/event/feature/src/lib/event.component.ts
@@ -1,5 +1,8 @@
 import { Component } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { ModalController } from '@ionic/angular';
+import { LeaderboardComponent } from '@encompass/app/leaderboard/feature';
+
 
 @Component({
   selector: 'event',
@@ -7,7 +10,7 @@ import { FormBuilder, FormGroup, Validators } from '@angular/forms';
   styleUrls: ['./event.component.scss'],
 })
 export class EventPage {
-  constructor(private formBuilder: FormBuilder) {}
+  constructor(private formBuilder: FormBuilder,private modalController: ModalController,) {}
 
   categories = ['Action', 'Comedy', 'Fantasy'];
   events = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17];
@@ -20,6 +23,18 @@ export class EventPage {
   checkInput() {
     return;
   }
+
+  async openPopup() {
+    const modal = await this.modalController.create({
+      component: LeaderboardComponent,
+      cssClass: 'custom-modal',
+      componentProps: {
+      },
+    });
+
+    return await modal.present();
+  }
+
   messageForm = this.formBuilder.group({
     messageInput: ['', Validators.maxLength(150)],
   });
