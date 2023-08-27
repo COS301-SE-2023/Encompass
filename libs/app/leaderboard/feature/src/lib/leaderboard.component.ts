941a3591ac4dcd9318abae1dca0cf7643a14c679
RoninBrookes <126901461+RoninBrookesTuks@users.noreply.github.com>
2023-08-27 17:07:05 +0200

    Leaderboard created

---
+++ libs/app/leaderboard/feature/src/lib/leaderboard.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'leaderboard',
+  templateUrl: './leaderboard.component.html',
+  styleUrls: ['./leaderboard.component.css']
+})
+export class LeaderboardComponent {
+
+}
