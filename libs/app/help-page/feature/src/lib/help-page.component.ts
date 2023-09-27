5a2ad2c8cf759734d4632dbbfd88c34e670407eb
Julianna-Venter <juliannaventer@gmail.com>
2023-09-27 15:28:38 +0200

    help page

--- libs/app/help-page/feature/src/lib/help-page.component.ts
+++ libs/app/help-page/feature/src/lib/help-page.component.ts
@@ -3,8 +3,17 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'help-page',
   templateUrl: './help-page.component.html',
-  styleUrls: ['./help-page.component.css']
+  styleUrls: ['./help-page.component.scss']
 })
 export class HelpPagePage {
+  mobileview = false;
 
+  ngOnInit() {
+    this.updateMobileView();
+    window.addEventListener('resize', this.updateMobileView.bind(this));
+  }
+
+  updateMobileView() {
+    this.mobileview = window.innerWidth <= 992;
+  }
 }
